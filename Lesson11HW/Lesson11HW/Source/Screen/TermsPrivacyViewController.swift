e36c6ecb202c8301692fe5a113f43b621a192218
IgorBerezhnoi <iberezhnoi@qualium-systems.com>
2024-03-27 21:54:54 +0100

    Implementate Terms & Privacy Screen

--- Lesson11HW/Lesson11HW/Source/Screen/TermsPrivacyViewController.swift
+++ Lesson11HW/Lesson11HW/Source/Screen/TermsPrivacyViewController.swift
@@ -9,8 +9,46 @@ import UIKit
 
 class TermsPrivacyViewController: BaseViewController {
     
+    @IBOutlet weak var textView: UITextView!
+    
+    @IBOutlet weak var agreeButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        agreeButton.isEnabled = false
+        
+        configureTextView()
     }
 }
 
+// MARK: - Private
+private extension TermsPrivacyViewController {
+    private func setButtonEnabledStyle() {
+        agreeButton.isEnabled = true
+        agreeButton.tintColor = UIColor.systemBlue
+        agreeButton.setTitleColor(UIColor.white, for: .normal)
+    }
+    
+    private func configureTextView() {
+        textView.isEditable = false
+        textView.isSelectable = true
+        textView.textContainerInset = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 20)
+        
+        textView.delegate = self
+    }
+}
+
+// MARK: - UITextViewDelegate
+extension TermsPrivacyViewController: UITextViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let offsetY = scrollView.contentOffset.y
+        let contentHeight = scrollView.contentSize.height
+        let frameHeight = scrollView.frame.size.height
+        
+        // Check if the user has scrolled to the bottom
+        if !agreeButton.isEnabled && offsetY >= contentHeight - frameHeight {
+            setButtonEnabledStyle()
+        }
+    }
+}
